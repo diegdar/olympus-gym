5e4d406d2c3e864fdbba5e5c65fe89ea0da7c592
diegdar <diegdarch@hotmail.com>
2025-03-18 20:12:08 +0100

    feat: add GitHub authentication controller and update users table to allow nullable passwords

---
+++ app/Http/Controllers/Auth/GitHubAuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GitHubAuthController extends Controller
+{
+    public function redirectToGitHub()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function handleGitHubCallback()
+    {
+        $githubUser = Socialite::driver('github')->user();
+
+        $user = User::updateOrCreate(
+        ['email' => $githubUser->email],
+            [
+                        'name' => $githubUser->nickname,
+                        'email' => $githubUser->email,
+                    ]
+        );
+
+        Auth::login($user);
+
+        return view(route('dashboard', absolute: false));
+    }
+}
