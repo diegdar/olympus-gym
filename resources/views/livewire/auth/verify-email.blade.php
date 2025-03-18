a9f7b78a2f3426dce9bc1ca2723cfea92483076d
diegdar <diegdarch@hotmail.com>
2025-03-18 17:35:59 +0100

    feat: customize Email verify notifications translate them to Spanish.

--- resources/views/livewire/auth/verify-email.blade.php
+++ resources/views/livewire/auth/verify-email.blade.php
@@ -1,21 +1,21 @@
 <div class="mt-4 flex flex-col gap-6">
     <flux:text class="text-center">
-        {{ __('Please verify your email address by clicking on the link we just emailed to you.') }}
+        {{ __('Por favor, verifique su dirección de correo electrónico haciendo clic en el enlace que le hemos enviado por correo electrónico.') }}
     </flux:text>
 
     @if (session('status') == 'verification-link-sent')
         <flux:text class="text-center font-medium !dark:text-green-400 !text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+            {{ __('Se ha enviado un nuevo enlace de verificación a la dirección de correo electrónico que proporcionaste durante el registro.') }}
         </flux:text>
     @endif
 
     <div class="flex flex-col items-center justify-between space-y-3">
         <flux:button wire:click="sendVerification" variant="primary" class="w-full">
-            {{ __('Resend verification email') }}
+            {{ __('Reenviar correo de verificación') }}
         </flux:button>
 
         <flux:link class="text-sm cursor-pointer" wire:click="logout">
-            {{ __('Log out') }}
+            {{ __('Cerrar sesión') }}
         </flux:link>
     </div>
 </div>
