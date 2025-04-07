654e64584bd0edfa417ec644991caf7032f69cad
diegdar <diegdarch@hotmail.com>
2025-04-07 11:34:42 +0200

    feat: add phone and birthday fields in RegistrationFormTest

--- tests/Feature/Auth/RegistrationFormTest.php
+++ tests/Feature/Auth/RegistrationFormTest.php
@@ -22,23 +22,6 @@ class RegistrationFormTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_it_requires_valid_inputs()
-    {
-        Livewire::test(Register::class)
-            ->set('name', '')
-            ->set('email', 'invalid-email')
-            ->set('password', 'short')
-            ->set('password_confirmation', 'mismatch')
-            ->set('privacy', false)
-            ->call('register')
-            ->assertHasErrors([
-                'name' => 'required',
-                'email' => 'email',
-                'password' => 'confirmed',
-                'privacy' => 'accepted',
-            ]);
-    }
-
     public function test_it_registers_a_user_successfully()
     {
         Event::fake();// disable the events and pretends they were triggered
@@ -46,6 +29,8 @@ class RegistrationFormTest extends TestCase
         Livewire::test(Register::class)
             ->set('name', 'Joe Doe')
             ->set('email', 'joe.doe@example.com')
+            ->set('phone', '1234567890')
+            ->set('birthday', '2000-01-01')
             ->set('fee', 'monthly')
             ->set('password', 'SecurePassword123!')
             ->set('password_confirmation', 'SecurePassword123!')
@@ -64,5 +49,26 @@ class RegistrationFormTest extends TestCase
         $this->assertTrue(Hash::check('SecurePassword123!', $user->password));
 
         Event::assertDispatched(Registered::class);// check if the event was dispatched 
+    }    
+
+    public function test_it_requires_valid_inputs()
+    {
+        Livewire::test(Register::class)
+            ->set('name', '')
+            ->set('email', 'invalid-email')
+            ->set('password', 'short')
+            ->set('phone', 'this-is-not-a-phone-number')
+            ->set('birthday', 'not-a-date')
+            ->set('password_confirmation', 'mismatch')
+            ->set('privacy', false)
+            ->call('register')
+            ->assertHasErrors([
+                'name' => 'required',
+                'email' => 'email',
+                'password' => 'confirmed',
+                'privacy' => 'accepted',
+            ]);
     }
+
+
 }
