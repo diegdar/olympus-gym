66f28f014ea0bf9bf9cc29f23a9065cfeb2549e2
diegdar <diegdarch@hotmail.com>
2025-04-08 19:26:39 +0200

    feat(subscription): add SubscriptionSeeder and SubscriptionUserSeeder for seeding subscription data

---
+++ database/seeders/SubscriptionSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Subscription;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubscriptionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Subscription::create([
+            'fee' => 'monthly',
+            'description' => 'Suscripción mensual',
+            'price' => 50,
+            'duration' => 1
+        ]);
+
+        Subscription::create([
+            'fee' => 'quarterly',
+            'description' => 'Suscripción trimestral',
+            'price' => 135,
+            'duration' => 3
+        ]);
+
+        Subscription::create([
+            'fee' => 'yearly',
+            'description' => 'Suscripción anual',
+            'price' => 480,
+            'duration' => 12
+        ]);
+    }
+}
