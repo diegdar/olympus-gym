8369fc5c321fa63d8510749cb72261e25937a25c
diegdar <diegdarch@hotmail.com>
2025-04-08 19:35:16 +0200

    feat(register): refactor registration logic to include subscription handling and update password confirmation binding

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -1,23 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Livewire\Auth;
 
-use App\Models\User;
+use App\Models\{
+    Subscription,
+    User,
+};
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
-use Livewire\Attributes\Layout;
-use Livewire\Component;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\{
+    Auth,
+    DB,
+    Hash,
+};
+use Illuminate\Validation\Rules;
+use Livewire\{
+    Attributes\Layout,
+    Component,
+};
 
 #[Layout('components.layouts.auth')]
 class Register extends Component
 {
     public string $name = '';
-    public string $email = '';
-    public string $phone = '';
-    public string $birthday = '';    
+    public string $email = '';   
     public string $password = '';
     public string $fee = '';
     public string $password_confirmation = '';
@@ -30,27 +38,42 @@ class Register extends Component
         $this->registerMessage = $request->input('registerMessage', '');
     }
 
+
     public function register(): void
     {
         $validated = $this->validate($this->rules());
-
+    
         $validated['password'] = Hash::make($validated['password']);
-
-        event(new Registered($user = User::create($validated)));
-
-        Auth::login($user);
-
+    
+        DB::transaction(function () use ($validated) {
+            $user = User::create($validated);
+    
+            $this->subscribeOrFail($user, $validated['fee']);
+    
+            event(new Registered($user));
+            Auth::login($user);
+        });
+    
         $this->redirect(route('dashboard', absolute: false), navigate: true);
     }
-
+    
     protected function rules(): array
     {
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-            'fee' => ['required', 'in:monthly,quarterly,yearly'],
+            'fee' => ['required', 'string', 'exists:subscriptions,fee'],
             'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
             'privacy' => ['required', 'accepted'],
         ];
+    }
+
+    private function subscribeOrFail(User $user, string $subscriptionValue)
+    {
+        $subscription = Subscription::where('fee', $subscriptionValue)->first();
+
+        if(!$user->subscribeTo($subscription)) {
+            return redirect()->route('register', ['registerMessage'=>'Error al suscribirse']);
+        }        
     }
 }
