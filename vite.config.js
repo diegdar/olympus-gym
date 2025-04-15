d4d336283804fc17ed43850da9b55ee9e3674883
diegdar <diegdarch@hotmail.com>
2025-04-15 10:41:14 +0200

    feat(auth): implement AdminLTE authentication controllers and views for login, registration, password reset, and email verification

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,14 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
-                'resources/js/navbar.js',
-                'resources/js/passwordVisibility.js',
             ],
-            refresh: [`resources/views/**/*`],
+            refresh: true,
         }),
-        tailwindcss(),
     ],
-    server: {
-        cors: true,
-    },
 });
