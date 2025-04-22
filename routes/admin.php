1559291def64ae383fd3926114311d38683fd603
diegdar <diegdarch@hotmail.com>
2025-04-22 20:43:30 +0200

    feat(roles): implement RoleController with resource methods for role management

--- routes/admin.php
+++ routes/admin.php
@@ -2,9 +2,13 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('users', UserController::class)     
     ->only(['index', 'edit', 'update', 'destroy'])
     ->names('admin.users');
+
+Route::resource('roles', RoleController::class)
+    ->names('admin.roles');
