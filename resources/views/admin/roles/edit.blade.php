09606472d280384604e143c88a1c930a6cea6ca7
diegdar <diegdarch@hotmail.com>
2025-04-24 20:25:49 +0200

    fix(roles): correct heading in role edit view and ensure form structure is intact

--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -1,6 +1,11 @@
 <x-layouts.app>
 
     <x-slot name="title">Editar roles</x-slot>
-    <h1>Editar roles</h1>
-
+    <h1>Editar role</h1>
+    <!-- form -->
+    <form action="{{ route('admin.roles.update', $role) }}" method="post" class="mt-4">
+        @csrf
+        @method('PUT')
+        @include('admin.roles.partials.form')
+    </form>
 </x-layouts.app>
