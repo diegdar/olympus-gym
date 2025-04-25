cd67a12d0ef0b306d0a0be08e9a10bca5ae09a2d
diegdar <diegdarch@hotmail.com>
2025-04-25 19:49:09 +0200

    feat(activities): create Activity model and migration for activities table

---
+++ app/Models/Activity.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'duration',
+    ];    
+}
