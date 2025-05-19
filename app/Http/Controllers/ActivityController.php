c478404ccc8231f8d2e03c82d5e73587ad07ade9
diegdar <diegdarch@hotmail.com>
2025-05-19 12:48:53 +0200

    feat(controllers, tests): add ActivityController and ActivityListTest for managing and testing activity listings

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Services\ActivityScheduleListService;
