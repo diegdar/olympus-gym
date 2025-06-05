fc0838ad65eb2f844d9d940f1ad974872335affa
diegdar <diegdarch@hotmail.com>
2025-06-05 10:41:10 +0200

    feat(tests): refactor RoleListTest to improve role permission handling and visibility checks

--- tests/Feature/Admin/Roles/RoleListTest.php
+++ tests/Feature/Admin/Roles/RoleListTest.php
@@ -15,13 +15,24 @@ class RoleListTest extends TestCase
 {
     use RefreshDatabase, RoleTestHelper;
 
-    protected array $authorizedRoles;
+    // authorized roles
+    protected array $authRolesForRolesList;
+    protected array $authRolesForCreateRole;    
+    protected array $authRolesForEditRole;    
+    protected array $authRolesForDestroyRole;    
+    // unauthorized roles
+    protected array $unauthRolesForRolesList;
+    protected array $unauthRolesForCreateRole;
+    protected array $unauthRolesForEditRole;
+    protected array $unauthRolesForDestroyRole;    
 
-    protected array $unauthorizedRoles;
-
-    protected const PERMISSION_NAME = 'admin.roles.index';
-
-    protected const ROUTE_LIST_ROLES = 'admin.roles.index';
+    // Permissions
+    protected const PERMISSION_LIST_ROLES = 'admin.roles.index';
+    protected const PERMISSION_CREATE_ROLE = 'admin.roles.create';
+    protected const PERMISSION_EDIT_ROLE = 'admin.roles.edit';
+    protected const PERMISSION_DESTROY_ROLE = 'admin.roles.destroy';
+    // Routes
+    protected const ROUTE_ROLE_INDEX = 'admin.roles.index';
     protected const ROUTE_CREATE_ROLE_VIEW = 'admin.roles.create';
     protected const ROUTE_STORE_ROLE = 'admin.roles.store';
     protected const ROUTE_EDIT_ROLE_VIEW = 'admin.roles.edit';
@@ -32,8 +43,14 @@ class RoleListTest extends TestCase
     {
         parent::setUp();
         $this->seed(RoleSeeder::class);
-        $this->authorizedRoles = $this->getAuthorizedRoles(self::PERMISSION_NAME);
-        $this->unauthorizedRoles = $this->getUnauthorizedRoles(self::PERMISSION_NAME);           
+        $this->authRolesForRolesList = $this->getAuthorizedRoles(self::PERMISSION_LIST_ROLES);
+        $this->unauthRolesForRolesList = $this->getUnauthorizedRoles(self::PERMISSION_LIST_ROLES);
+        $this->authRolesForCreateRole = $this->getAuthorizedRoles(self::PERMISSION_CREATE_ROLE);
+        $this->unauthRolesForCreateRole = $this->getUnauthorizedRoles(self::PERMISSION_CREATE_ROLE);
+        $this->authRolesForEditRole = $this->getAuthorizedRoles(self::PERMISSION_EDIT_ROLE);
+        $this->unauthRolesForEditRole = $this->getUnauthorizedRoles(self::PERMISSION_EDIT_ROLE);
+        $this->authRolesForDestroyRole = $this->getAuthorizedRoles(self::PERMISSION_DESTROY_ROLE);
+        $this->unauthRolesForDestroyRole = $this->getUnauthorizedRoles(self::PERMISSION_DESTROY_ROLE);
     }
 
     /**
@@ -42,16 +59,16 @@ class RoleListTest extends TestCase
      * @param string $roleName The name of the role to assign to the user.
      * @return TestResponse The response from accessing the role index page.
      */
-    private function getIndexAs(string $roleName): TestResponse
+    private function getRolesListAs(string $roleName): TestResponse
     {
-        return $this->actingAsRole($roleName)->get(route(self::ROUTE_LIST_ROLES));
+        return $this->actingAsRole($roleName)->get(route(self::ROUTE_ROLE_INDEX));
     }
     
     public function test_users_with_index_permission_can_see_role_list()
     {
         $roles = Role::all();
-        foreach ($this->authorizedRoles as $authorizedRole) {
-            $response = $this->getIndexAs($authorizedRole);
+        foreach ($this->authRolesForRolesList as $authorizedRole) {
+            $response = $this->getRolesListAs($authorizedRole);
 
             $response->assertStatus(200)
                      ->assertSee('Lista de roles')
@@ -65,8 +82,8 @@ class RoleListTest extends TestCase
     
     public function test_users_without_index_permission_get_403()
     {
-        foreach ($this->unauthorizedRoles as $unauthorizedRole) {
-            $response = $this->getIndexAs($unauthorizedRole);
+        foreach ($this->unauthRolesForRolesList as $unauthorizedRole) {
+            $response = $this->getRolesListAs($unauthorizedRole);
 
             $response->assertStatus(403)
                      ->assertDontSee('Lista de roles')
@@ -75,50 +92,83 @@ class RoleListTest extends TestCase
         }
     }
     
-    public function test_create_button_visibility_depends_on_permission()
+    public function test_create_button_is_visible_depends_on_permission()
     {
-        // super-admin has create permission
-        $this->getIndexAs('super-admin')
-             ->assertStatus(200)
-             ->assertSee('Crear role')
-             ->assertSee(route(self::ROUTE_CREATE_ROLE_VIEW));
+        foreach ($this->authRolesForCreateRole as $role) {
+            $this->getRolesListAs($role)
+                 ->assertStatus(200)
+                 ->assertSeeText('Crear role')
+                 ->assertSee(route(self::ROUTE_CREATE_ROLE_VIEW), false);
+        }
 
-        // admin does not
-        $this->getIndexAs('admin')
-             ->assertStatus(200)
-             ->assertDontSee('Crear role')
-             ->assertDontSee(route(self::ROUTE_CREATE_ROLE_VIEW));
+        foreach ($this->unauthRolesForCreateRole as $role) {
+            $response = $this->getRolesListAs($role);
+            if ($response->status() === 200) {
+                $response->assertDontSeeText('Crear sala');
+                $response->assertDontSee(route(self::ROUTE_CREATE_ROLE_VIEW), false);
+            } else {
+                $response->assertStatus(403);
+            }
+        }
     }
     
-    public function test_action_columns_visibility_depends_on_permissions()
+    public function test_edit_button_is_visible_depends_on_permission(): void
     {
-        // super-admin
-        $this->getIndexAs('super-admin')
-             ->assertStatus(200)
-             ->assertSee('Acción')
-             ->assertSee(route(self::ROUTE_EDIT_ROLE_VIEW, 1))
-             ->assertSee(route(self::ROUTE_DESTROY_ROLE, 1));
+        foreach ($this->authRolesForEditRole as $role) {
+            $response = $this->getRolesListAs($role)
+                 ->assertStatus(200)
+                 ->assertSeeText('Acción')
+                 ->assertSee(
+                    route(
+                        self::ROUTE_EDIT_ROLE_VIEW,
+                     1),
+                     false
+                    );
+        }
 
-        // admin
-        $this->getIndexAs('admin')
-             ->assertStatus(200)
-             ->assertDontSee('Acción')
-             ->assertDontSee(route(self::ROUTE_EDIT_ROLE_VIEW, 1))
-             ->assertDontSee(route(self::ROUTE_DESTROY_ROLE, 1));
+        foreach ($this->unauthRolesForEditRole as $role) {
+            $response = $this->getRolesListAs($role);
+            if ($response->status() === 200) {
+                $response->assertDontSeeText('Acción')
+                         ->assertDontSee(
+                            route(
+                                    self::ROUTE_EDIT_ROLE_VIEW,
+                                     1),
+                                       false
+                            );
+            } else {
+                $response->assertStatus(403);
+            }
+        }        
     }
-    
-    public function test_flash_message_is_displayed_if_present_in_session()
-    {
-        $response = $this->actingAsRole('super-admin')        
-            ->withSession(['msg' => 'Operación exitosa.'])
-            ->get(route(self::ROUTE_LIST_ROLES));
 
-        $response->assertStatus(200);
-    }
-    
-    public function test_no_flash_message_when_session_empty()
+    public function test_destroy_button_is_visible_depends_on_permission(): void
     {
-        $this->getIndexAs('super-admin')
-             ->assertStatus(200);
-    }
+        foreach ($this->authRolesForDestroyRole as $role) {
+            $response = $this->getRolesListAs($role)
+                 ->assertStatus(200)
+                 ->assertSeeText('Acción')
+                 ->assertSee(
+                    route(
+                        self::ROUTE_DESTROY_ROLE,
+                     1),
+                     false
+                    );
+        }
+
+        foreach ($this->unauthRolesForDestroyRole as $role) {
+            $response = $this->getRolesListAs($role);
+            if ($response->status() === 200) {
+                $response->assertDontSeeText('Acción')
+                         ->assertDontSee(
+                            route(
+                                    self::ROUTE_DESTROY_ROLE,
+                                     1),
+                                       false
+                            );
+            } else {
+                $response->assertStatus(403);
+            }
+        }        
+    }    
 }
