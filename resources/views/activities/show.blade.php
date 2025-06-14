16eb2b4f529f2b046347df8b7616b57d54deae40
diegdar <diegdarch@hotmail.com>
2025-06-14 13:52:55 +0200

    feat(views): add show view for activity details

---
+++ resources/views/activities/show.blade.php
@@ -0,0 +1,22 @@
+<x-layouts.app>
+    <h1>sala: {{ $activity->name }}</h1>
+    <!-- duration -->
+    <article class="flex flex-wrap gap-2n mt-5 mx-3">
+        <div class="flex flex-col sm:flex-row">
+            <span class="font-bold dark:text-red-300">
+                Duración:</span>
+            <p class="text-gray-700 dark:text-gray-300 text-lg font-semibold mb-2 ml-3" id="description">
+                {{ $activity->duration }} minutos</p>
+        </div>
+    </article>
+    <!-- descripcion -->
+    <article class="flex flex-wrap gap-2n mt-5 mx-3">
+        <div class="flex flex-col sm:flex-row">
+            <span class="font-bold dark:text-red-300">
+                Descripción:</span>
+            <p name="description" class="text-gray-700 dark:text-gray-300 text-lg font-semibold mb-2 ml-3" id="description">
+                {{ $activity->description }}</p>
+        </div>
+    </article>
+
+</x-layouts.app>
