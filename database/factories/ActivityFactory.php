bfc811e19965a959b4bfdade9d8073d2f1ab5af0
diegdar <diegdarch@hotmail.com>
2025-06-17 11:22:21 +0200

    feat(factories): update name generation in ActivityFactory and RoomFactory to use longer text

--- database/factories/ActivityFactory.php
+++ database/factories/ActivityFactory.php
@@ -18,7 +18,8 @@ class ActivityFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->unique()->word(),
+            'name' => fake()->unique()
+                        ->text(rand(5, 15)),
             'description' => fake()->sentence(),
             'duration' => fake()->randomElement(['30', '45', '60', '90']),
         ];
