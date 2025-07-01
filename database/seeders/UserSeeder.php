33345bd5c7c27c23b8022a9f5e20dae2d2129a51
diegdar <diegdarch@hotmail.com>
2025-07-01 09:56:38 +0200

    feat(seeders): update UserSeeder to use environment variables for member credentials and add activity schedule permissions in RoleSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -28,9 +28,9 @@ class UserSeeder extends Seeder
         ])->assignRole('admin');        
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('Password%123')
+            'name' => env('MEMBER_NAME'),
+            'email' => env('MEMBER_EMAIL'),
+            'password' => Hash::make(env('MEMBER_PASSWORD'))
         ])->assignRole('member'); 
         
         User::factory(15)->create()
