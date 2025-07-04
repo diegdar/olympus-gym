a94eaea0973e905f1cb48d14b8be4ca5075bcb4c
diegdar <diegdarch@hotmail.com>
2025-07-04 13:44:41 +0200

    feat(activity): implement form request for activity validation

---
+++ app/Http/Requests/StoreActivityFormRequest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreActivityFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:3', 'max:50', 'unique:activities,name'],
+            'description' => ['nullable', 'min:10', 'string', 'max:2000'],
+            'duration' => ['required', 'integer', 'in:30,45,60'],            
+        ];
+    }
+}
