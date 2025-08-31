b6f1eceac6a4cec741b60e144f3c4f79bc0558ab
diegdar <diegdarch@hotmail.com>
2025-08-31 11:12:26 +0200

    feat: implement StoreSubscriptionService for subscription management and enhance subscription view with change functionality

--- resources/views/users/subscription.blade.php
+++ resources/views/users/subscription.blade.php
@@ -13,17 +13,56 @@
     @endif
 
     {{-- Subscription details --}}
-    <div class="bg-white dark:bg-gray-800 shadow-md rounded-lg p-6">
-        <p class="mt-2 flex flex-col sm:flex-row"><strong class="font-bold dark:text-red-300 mr-2">Usuario:</strong> {{ $user->name }}
+    <div class="bg-white dark:bg-gray-800 shadow-md rounded-lg pl-5 py-2">
+        <p class="mt-2 flex flex-col sm:flex-row"><strong class="font-bold dark:text-red-300 mr-2">Usuario:</strong>
+            {{ $user->name }}
         </p>
-        <p class="mt-2 flex flex-col sm:flex-row"><strong class="font-bold dark:text-red-300 mr-2">Cuota:</strong>
-            {{ $subscription->fee_translated ?? 'No asignada' }}
+        <p class="mt-2 flex flex-col sm:flex-row"><strong class="font-bold dark:text-red-300 mr-2">Fecha de
+                inicio:</strong>
+            {{ $currentSubscription->pivot->start_date_formatted ?? 'No asignada' }}
         </p>
-        <p class="mt-2 flex flex-col sm:flex-row"><strong class="font-bold dark:text-red-300 mr-2">Fecha de inicio:</strong>
-            {{ $subscription->pivot->start_date_formatted ?? 'No asignada' }}
+        <p class="mt-2 flex flex-col sm:flex-row"><strong class="font-bold dark:text-red-300 mr-2">Fecha de
+                finalización:</strong>
+            {{ $currentSubscription->pivot->end_date_formatted ?? 'No asignada' }}
         </p>
-        <p class="mt-2 flex flex-col sm:flex-row"><strong class="font-bold dark:text-red-300 mr-2">Fecha de finalización:</strong>
-            {{ $subscription->pivot->end_date_formatted ?? 'No asignada' }}
-        </p>
+        {{-- FEE --}}
+        <div class="flex mt-2">
+            <p class="flex flex-col sm:flex-row"><strong class="font-bold dark:text-red-300 mr-2">Cuota:</strong>
+                {{ $currentSubscription->fee_translated ?? 'No asignada' }}
+            </p>
+            <article class="flex flex-wrap sm:flex-nowrap ms-3 sm:ms-10">
+                <label class="font-bold dark:text-yellow-400 mr-2 whitespace-nowrap" for="subscription_id">
+                    Cambiar por:
+                </label>
+                <form action="{{ route('member.subscription.update') }}" method="POST" class="flex flex-wrap gap-2">
+                    @csrf
+                    @method('PUT')
+                    <div>
+                        <select name="subscription_id" id="subscription_id"
+                            class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300 mt-1 sm:mt-0">
+                            <option value="" class="w-auto">-Seleccione Suscripción-</option>
+                            @foreach ($subscriptions as $subscription)
+                                <option value="{{ $subscription->id }}">
+                                    {{ $subscription->fee_translated }}
+                                </option>
+                            @endforeach
+                        </select>
+                        @error('subscription_id')
+                            <span class="block text-red-500 text-sm dark:text-red-400 mt-1">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                    {{-- Send button --}}
+                    <div class="flex items-start">
+                        <button type="submit"
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-1 sm:mt-0">
+                            Cambiar
+                        </button>
+                    </div>
+                </form>
+            </article>
+        </div>
     </div>
+    @vite('resources/js/messageTransition.js')
 </x-layouts.app>
