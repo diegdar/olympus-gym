ad058e657d759183d2940fbe7fe8e5bffe5080c6
Diego Chacon <133038702+diegdar@users.noreply.github.com>
2025-09-24 17:35:51 +0000

    feat: replace footer section with reusable Footer component

--- resources/views/components/layouts/app/general.blade.php
+++ resources/views/components/layouts/app/general.blade.php
@@ -29,14 +29,7 @@
     </main>
 
     <!-- Footer -->
-    <footer class="text-center py-4 bg-gray-500 text-white dark:bg-bg_darkMode dark:text-white">
-        <p>
-            &copy; Developed by: 
-            <a href="https://diegochacondev.es/" target="_blank" rel="noopener noreferrer" class="underline hover:text-blue-400">
-                Diego Chacon Delgado
-            </a>.
-        </p>
-    </footer>
+    <x-footer></x-footer>
 
 </body>
 @vite('resources/js/navbar.js')
