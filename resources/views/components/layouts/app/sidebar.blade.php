26c0b2fa5708bb7f31e1e48defb6472875a95838
Diego Chacon <133038702+diegdar@users.noreply.github.com>
2025-10-20 08:11:57 +0000

    feat(settings): localize sidebar and settings interface strings to Spanish
    - Translate sidebar menu items (Settings, Log Out) to Spanish
    - Update settings navigation labels (Profile, Password, Appearance) to Spanish
    - Localize settings heading and subheading text
    - Add minor styling adjustments to settings heading layout

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -150,7 +150,7 @@
                 <flux:menu.separator />
 
                 <flux:menu.radio.group>
-                    <flux:menu.item href="/settings/profile" icon="cog" wire:navigate>{{ __('Settings') }}
+                    <flux:menu.item href="/settings/profile" icon="cog" wire:navigate>{{ __('Configuración') }}
                     </flux:menu.item>
                 </flux:menu.radio.group>
 
@@ -159,7 +159,7 @@
                 <form method="POST" action="{{ route('logout') }}" class="w-full">
                     @csrf
                     <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
-                        {{ __('Log Out') }}
+                        {{ __('Cerrar Sesion') }}
                     </flux:menu.item>
                 </form>
             </flux:menu>
--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -1,9 +1,9 @@
 <div class="flex items-start max-md:flex-col">
     <div class="mr-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>
-            <flux:navlist.item :href="route('settings.profile')" wire:navigate>{{ __('Profile') }}</flux:navlist.item>
-            <flux:navlist.item :href="route('settings.password')" wire:navigate>{{ __('Password') }}</flux:navlist.item>
-            <flux:navlist.item :href="route('settings.appearance')" wire:navigate>{{ __('Appearance') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.profile')" wire:navigate>{{ __('Perfil') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.password')" wire:navigate>{{ __('Contraseña') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.appearance')" wire:navigate>{{ __('Apariencia') }}</flux:navlist.item>
             <flux:navlist.item :href="route('settings.two-factor')" wire:navigate>{{ __('Verificación en dos pasos') }}</flux:navlist.item>
         </flux:navlist>
     </div>
